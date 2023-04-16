403e3796c9bf6499aa651ca453e2a387a619378a
Imagoprint <126903816+Imagoprint@users.noreply.github.com>
2023-04-16 22:58:36 +0300

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,25 @@
+import React from 'react';
+import { TiDelete } from 'react-icons/ti';
+import { IoMdAddCircle } from 'react-icons/io';
+import { HiMinusCircle } from 'react-icons/hi';
+import { AppContext } from '../context/AppContext';
+
+
+
+const ExpenseItem = (props) => {
+    const { currency} = useContext(AppContext);
+    return (
+        <tr>
+            <td>{props.name}</td>
+            <td>{currency}{props.cost}</td>
+            <td><IoMdAddCircle size='2em' color='green'></IoMdAddCircle></td>
+            <td><HiMinusCircle size='2em' color='red'></HiMinusCircle></td>
+            <td><TiDelete size='1em'></TiDelete></td>
+            </tr>
+    );
+};
+
+export default ExpenseItem;
+
+
 
