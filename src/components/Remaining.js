a7d110e7d833494f1321fcf3b7e955cd21875624
Imagoprint <126903816+Imagoprint@users.noreply.github.com>
2023-04-16 22:59:31 +0300

    Update Remaining.js

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import App from '../App';
+
+const Remaining = () => {
+    return (
+<div className='alert alert-success'>
+    <span>Remaining: £1040</span>
+</div>
+    );
+};
+
+export default Remaining;
 
