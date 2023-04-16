4b459824d6e344cecc3f76d0ff87e0aa0c3ec4e1
Imagoprint <126903816+Imagoprint@users.noreply.github.com>
2023-04-16 22:59:12 +0300

    Update ExpenseTotal.js

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,1 +1,16 @@
+import React from 'react';
+import App from '../App';
+import { AppContext } from '../context/AppContext';
+
+
+const ExpenseTotal = () => {
+    return (
+<div className='alert alert-primary'>
+    <span>Spent so far: £960</span>
+</div>
+    );
+};
+
+export default ExpenseTotal;
+
 
