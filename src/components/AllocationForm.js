15ac21b4488ba19a20fb924c5eab4c8ae536875d
Imagoprint <126903816+Imagoprint@users.noreply.github.com>
2023-04-16 22:57:57 +0300

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,1 +1,28 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const AllocationForm = () => {
+  const {dispatch } = useContext(AppContext);
+
+    const changeCurrency = (val)=>{
+            dispatch({
+                type: 'CHG_LOCATION',
+                payload: val,
+            })
+    }
+    
+
+  return (
+        <div className='alert alert-info'> Currency  {
+      <select name="currency" id="currency" onChange={event=>changeCurrency(event.target.value)}>
+        <option value="£">£ Pound</option>
+        <option value="$">$ Dollar</option>
+        <option value="€">€ Euro</option>
+        <option value="INR">₹ Ruppee</option>
+      </select>	
+      }	
+    </div>
+    );
+};
+
+export default AllocationForm;
